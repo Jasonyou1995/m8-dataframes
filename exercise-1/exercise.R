1bad720c5dabcd3fd5a702f81d0c055b5b38c96a
Jason You <jason.you1995@gmail.com>
2016-10-11 14:18:53 -0700

    ex2

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -2,25 +2,27 @@
 
 
 # Create a vector of everything you ate for breakfast
-
+breakfast <- c('orange juice', 'yogurt', 'grape')
 
 # Create a vector of everything you ate for lunch
-
+lunch <- c('pork', 'rice')
 
 # Create a list `meals` that has contains your breakfast and lunch
-
+all.food <- list(breakfast=breakfast, lunch=lunch)
 
 # Add a `dinner` index to your `meals` list that has what you plan to eat for dinner
-
+all.food[['dinner']] <- c('congee')
 
 # Extract your 'dinner' element from your list and save it in a vector called 'dinner'
-
+dinner <- all.food$dinner
 
 ### Bonus ### 
 # Create a list that has the number of items you ate for each meal
-
+numb.food <- lapply(all.food, length)
 
 # Write a function that adds pizza to every meal
-
-
+addPizza <- function(x) {
+    x <- c(x, 'pizza')
+}
 # Add pizza to every meal!
+addPizza(all.food)
--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -1,22 +1,22 @@
 # Exercise 2: Creating data frames
 
 # Create a vector of the number of points the Seahawks have scored in each game this season (google "Seahawks")
-
+score <- c(12, 9, 37, 17)
 
 # Create a vector of the number of points the Seahwaks have allowed to be scored against them in each game this season
-
+allow <- c(10, 3, 18, 27)
 
 # Combine your two vectors into a dataframe
-
+all <- data.frame(score=score, allow=allow)
 
 # Create a new column "diff" that is the difference in points
-
+all[['diff']] <- all$score - all$allow
 
 # Create a new column "won" which is TRUE if the Seahawks wom
-
+all[['won']] <- c(T, F, T, T)
 
 # Create a vector of the opponents
-
+all[['opponent']] <- !all[['won']]
 
 # Assign your dataframe rownames of their opponents
-
+row.names(all) <- c('Dolphines', 'Rams', '49ers', 'Jets')
